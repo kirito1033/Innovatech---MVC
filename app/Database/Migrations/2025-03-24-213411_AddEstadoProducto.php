955f3318fa7f8749c5b3eefd4bf795bece5d5c53
Sebastian1033 <111609725+kirito1033@users.noreply.github.com>
2025-03-24 18:00:44 -0500

    Se realiza los modelos y migraciones de estado producto, garantia, marca, permisos, sistemaOperativo, resolucion y modelos

---
+++ app/Database/Migrations/2025-03-24-213411_AddEstadoProducto.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddEstadoProducto extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'TINYINT',
+                'constraint'     => 3,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nom' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 30,
+                'null'       => false,
+            ],
+        ]);
+        
+        $this->forge->addKey('id', true); // Clave primaria
+        $this->forge->createTable('estado_producto', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('estado_producto', true);
+    }
+}
---
+++ app/Database/Migrations/2025-03-24-225104_AddModelos.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddModelos extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 10,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'Ruta' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 30,
+                'null'       => false,
+                'unique'     => true,
+            ],
+            'Descripción' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+                'null'       => true,
+            ],
+        ]);
+        
+        $this->forge->addKey('id', true); // Clave primaria
+        $this->forge->createTable('modelos', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('modelos', true);
+    }
+}
