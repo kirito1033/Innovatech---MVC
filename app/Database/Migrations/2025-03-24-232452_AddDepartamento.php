e87f9dc985d916fdcc3b9897f713ec1b29376d65
Sebastian1033 <111609725+kirito1033@users.noreply.github.com>
2025-03-24 19:05:49 -0500

    Se realiza los modelos y migraciones de departamento, ciudad, usuario, envio, pqrs, perdido, factura, facturaenvio, modelostol, permisosmolo

---
+++ app/Database/Migrations/2025-03-24-232452_AddDepartamento.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddDepartamento extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'TINYINT',
+                'constraint'     => 3,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nom' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 50,
+                'null'       => false,
+            ],
+        ]);
+        
+        $this->forge->addKey('id', true); // Clave primaria
+        $this->forge->createTable('departamento', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('departamento', true);
+    }
+}
